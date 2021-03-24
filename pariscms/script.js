83eb28afe4a524e03508199fc284bc3c76801ff7
Riedel, Rico Rouven Jan Nicolas (ID) <rico.riedel@id.unibe.ch>
2021-03-24 14:43:19 +0100

    IDEPSCHNITTSTELLEN-819: Merge responsibilities into on column

--- pariscms/script.js
+++ pariscms/script.js
@@ -2,25 +2,74 @@
 
 let repeatingValueClass = "repeating";
 
+let responsibilityTypeOrder = ["TV", "S0", "KV"];
+let responsibilityTypeMap = {
+    "TV": "Technik",
+    "S0": "Software",
+    "KV": "Konto"
+};
+
+class Department {
+    constructor(name, externId, responsibilities) {
+        this.name = name;
+        this.externId = externId;
+        this.responsibilities = responsibilities;
+    }
+}
+
 class Row {
-    constructor(person, responsibility) {
-        this.organizationalUnit = responsibility.type;
-        this.department = responsibility.nameGerman + " (" + responsibility.externId + ")";
+    constructor(person, department) {
+        this.responsibilities = department.responsibilities;
+        this.department = department.name + " (" + department.externId + ")";
         this.name = person.firstname + " " + person.lastname;
         this.phone = person.phone;
         this.email = person.email;
     }
 }
 
+function responsibilityComparer(left, right) {
+    return responsibilityTypeOrder.indexOf(left) - responsibilityTypeOrder.indexOf(right);
+}
+
+function departmentNameComparer(left, right) {
+    return left.localeCompare(right, undefined, {sensitivity: 'base'});
+}
+
 function flatten(array) {
     let flatArray = [];
 
     array.forEach((person) => {
+        // We make a map of each department, then we add each responsibility type to the corresponding department.
+        let departmentMap = {};
+
         person.responsibilities.forEach((responsibility) => {
-            flatArray.push(new Row(person, responsibility));
+            // This value is not visible and should an id or the shortest string possible.
+            let key = responsibility.shortNameGerman;
+
+            let department = departmentMap[key];
+
+            if (department === undefined) {
+                department = new Department(responsibility.nameGerman, responsibility.externId, []);
+                departmentMap[key] = department;
+            }
+
+            department.responsibilities.push(responsibility.type);
+        });
+
+        Object.values(departmentMap).forEach((department) => {
+            department.responsibilities.sort(responsibilityComparer);
+
+            flatArray.push(new Row(person, department));
         });
     });
     return flatArray;
+}
+
+function responsibilitiesRenderer(array, role) {
+    if (role === "display") {
+        return array.map(type => responsibilityTypeMap[type]).join(", ");
+    }
+    return array.join(";");
 }
 
 function draw() {
@@ -51,7 +100,7 @@ function tableReady() {
     let departmentSet = new Set();
     api.column("department:name").data().each((value) => departmentSet.add(value));
     // Add all departments to an array and sort. (Set doesn't sort alphabetically.)
-    let departmentArray = [...departmentSet].sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'}));
+    let departmentArray = [...departmentSet].sort(departmentNameComparer);
 
     $("#select-department").each(function () {
         this.addEventListener("change", function () {
@@ -72,11 +121,14 @@ $(document).ready(() => {
         processing: true,
         ajax: {url: "data.json", dataSrc: flatten},
         columns: [
-            {data: "organizationalUnit", name: "organizationalUnit", visible: false},
             {data: "department", name: "department"},
             {data: "name"},
             {data: "phone"},
-            {data: "email"}
+            {data: "email"},
+            {data: "responsibilities", name: "responsibilities"}
+        ],
+        columnDefs: [
+            {render: responsibilitiesRenderer, targets: 4},
         ],
         drawCallback: draw,
         initComplete: tableReady
@@ -100,7 +152,7 @@ $(document).ready(() => {
         }
 
         table
-            .column("organizationalUnit:name")
+            .column("responsibilities:name")
             .search(term, false, false, false)
             .draw();
     });
