58fddaee75bd9a004e045d64267f48a7be44a2fa
Christian Meier <christian.meier@unibe.ch>
2022-09-27 21:24:49 +0200

    CMSAPI: Revised newsevents

--- cmsapi/routers/newsevents.py
+++ cmsapi/routers/newsevents.py
@@ -1,72 +1,49 @@
 from fastapi import APIRouter
 from sqlmodel import Session, select, or_
-from datetime import datetime
+from datetime import datetime, timedelta
 from ..db import engine
 
 from ..models.newsevents import Newsbox, TeaserElement2022
 from ..models.zmsdefaults import ZMSSite
 from ..schemas import newsevents as schema
-from ..helpers import Lang, SiteType, get_attr_by_lang, strip_cmstest
+from ..helpers import Lang, get_attr_by_lang, strip_cmstest, local_timezone
 
 router = APIRouter(
     prefix="/v3",
     tags=["News and Events"])
 
 
-@router.get("/newsevents/sites", response_model=list[schema.Site])
-async def get_sites_with_news_events(
+@router.get("/news", response_model=list[schema.News])
+async def get_news(
         lang: Lang = Lang.de,
-        type: SiteType = SiteType.Home,
+        days_back: int = 365,
+        offset: int = 0,
+        limit: int = 50):
+    return retrieve("news", lang, days_back, offset, limit)
+
+
+@router.get("/events", response_model=list[schema.Event])
+async def get_events(
+        lang: Lang = Lang.de,
+        days_back: int = 365,
         offset: int = 0,
         limit: int = 10):
-    with Session(engine) as session:
-        statement = select(ZMSSite).join(Newsbox). \
-            where(ZMSSite.domain != ''). \
-            where(ZMSSite.type == type). \
-            where(or_(Newsbox.type == 'news', Newsbox.type == 'event')). \
-            where(get_attr_by_lang(lang, de=ZMSSite.active_de, en=ZMSSite.active_en, fr=ZMSSite.active_fr)). \
-            order_by(ZMSSite.domain). \
-            offset(offset).limit(limit).distinct()
-        results = session.exec(statement)
-        rtn = []
-        for res in results.all():
-            rtn.append(schema.Site.parse_obj({
-                'uuid': res.uuid,
-                'site': strip_cmstest(res.domain),
-                'type': res.type,
-                'title': get_attr_by_lang(lang, de=res.title_de, en=res.title_en, fr=res.title_fr),
-                'subdomain': res.domain,
-            }))
-        return rtn
+    return retrieve("event", lang, days_back, offset, limit)
 
 
-@router.get("/news/{site}", response_model=list[schema.News])
-async def get_news_by_site(
-        site: str,
-        lang: Lang = Lang.de,
-        offset: int = 0,
-        limit: int = 10):
-    return query_news_events("news", site, lang, offset, limit)
-
-
-@router.get("/events/{site}", response_model=list[schema.Event])
-async def get_events_by_site(
-        site: str,
-        lang: Lang = Lang.de,
-        offset: int = 0,
-        limit: int = 10):
-    return query_news_events("event", site, lang, offset, limit)
-
-
-def query_news_events(type, site, lang, offset, limit):
+def retrieve(type, lang, days_back, offset, limit):
     rtn = []
     statement = []
     with Session(engine) as session:
         for model in (TeaserElement2022, Newsbox):
             statement.append(
-                select(model).join(ZMSSite).
-                where(ZMSSite.domain == strip_cmstest(site, reverse=True)).
+                select(model, ZMSSite).join(ZMSSite).
                 where(model.type == type).
+                where(or_(ZMSSite.type == 'Home',
+                          ZMSSite.type == 'Fakultaet',
+                          ZMSSite.type == 'Institut',
+                          ZMSSite.type == 'Uniaktuell',  # to fetch UB
+                          ZMSSite.type == 'Microsite')).
                 where(get_attr_by_lang(lang,
                                        de=model.active_start_de,
                                        en=model.active_start_en,
@@ -75,6 +52,10 @@ def query_news_events(type, site, lang, offset, limit):
                                        de=model.active_end_de,
                                        en=model.active_end_en,
                                        fr=model.active_end_fr) <= datetime.now()).
+                where(get_attr_by_lang(lang,
+                                       de=model.lastmod_dt_de,
+                                       en=model.lastmod_dt_en,
+                                       fr=model.lastmod_dt_fr) >= datetime.now() - timedelta(days=days_back)).
                 where(get_attr_by_lang(lang,
                                        de=model.active_de,
                                        en=model.active_en,
@@ -93,6 +74,7 @@ def query_news_events(type, site, lang, offset, limit):
                                       en=Newsbox.lastmod_dt_en,
                                       fr=Newsbox.lastmod_dt_fr).desc()). \
             alias('alias')
+
         results = session.query(query).offset(offset).limit(limit)
 
         if type == 'news':
@@ -103,9 +85,26 @@ def query_news_events(type, site, lang, offset, limit):
         for res in results:
             rtn.append(cls.parse_obj({
                 'uuid': res.uuid,
-                'title': get_attr_by_lang(lang, de=res.title_de, en=res.title_en, fr=res.title_fr),
+                'title': get_attr_by_lang(lang,
+                                          de=res.title_de,
+                                          en=res.title_en,
+                                          fr=res.title_fr),
+                'topic': res.topic,
+                'start_dt':
+                    res.start_dt is not None and (
+                    local_timezone(res.start_dt) or None) or None,
+                'end_dt':
+                    res.end_dt is not None and (
+                    res.end_dt > datetime.fromisoformat('1970-01-01T00:00:00+00:00') and
+                    local_timezone(res.end_dt) or None) or None,
                 'level': res.level,
-                'lastmod': get_attr_by_lang(lang, de=res.lastmod_dt_de, en=res.lastmod_dt_en, fr=res.lastmod_dt_fr)
+                'lastmod': get_attr_by_lang(lang,
+                                            de=local_timezone(res.lastmod_dt_de),
+                                            en=local_timezone(res.lastmod_dt_en),
+                                            fr=local_timezone(res.lastmod_dt_fr)),
+                'site': strip_cmstest(res.domain),
+                'site_type': res.type_1,
+                'subdomain': res.domain,
             }))
 
     return rtn
--- cmsapi/schemas/newsevents.py
+++ cmsapi/schemas/newsevents.py
@@ -14,12 +14,22 @@ class Site(BaseModel):
 class News(BaseModel):
     uuid: UUID
     title: str
+    topic: str
     level: int
     lastmod: datetime
+    subdomain: str
+    site_type: str
+    site: str
 
 
 class Event(BaseModel):
     uuid: UUID
     title: str
+    topic: str
+    start_dt: datetime
+    end_dt: datetime | None
     level: int
     lastmod: datetime
+    subdomain: str
+    site_type: str
+    site: str
