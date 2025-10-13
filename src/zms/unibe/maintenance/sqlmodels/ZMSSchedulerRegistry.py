1148ed4ec47a5538cb9b6b469ae404b4b8d6ef71
Christian Meier <christian.meier@unibe.ch>
2025-10-13 18:35:49 +0200

    ZMSSchedulerRegistry: SQLModel to manage update tasks (#19)
    * feat(webhook): Added endpoints for scheduled updates
    
    Introduced ZMSSchedulerRegistry SQLModel and FastAPI endpoints to manage tasks in PostgreSQL. Extended the existing ZMSScheduler logic to process both ZMSRecordSet and PostgreSQL-based tasks with RESTful integration via zms-fastapi.
    
    Refs https://github.com/idasm-unibe-ch/zms-fastapi/issues/48

---
+++ src/zms/unibe/maintenance/sqlmodels/ZMSSchedulerRegistry.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from uuid import UUID
+from sqlmodel import SQLModel, Field, Column, DateTime
+
+from zms.unibe.utils.helpers import parse_uuid
+
+
+class ZMSSchedulerRegistry(SQLModel, table=True):
+    __table_args__ = {'extend_existing': True}
+    id: int | None = Field(default=None, primary_key=True)
+    task_uuid: UUID
+    task_path: str
+    task_title: str | None
+    exec_onchange: bool = False
+    exec_zms2sql: bool = False
+    exec_opensearch: bool = False
+    added_dt: datetime = Field(sa_column=Column(DateTime(timezone=True), nullable=True))
+    processed_dt: datetime | None = Field(sa_column=Column(DateTime(timezone=True), nullable=True))
+
+    @classmethod
+    def from_agenda(cls, obj):
+        mapping = {
+            'id':               None,
+            'task_uuid':        parse_uuid(obj._uid),
+            'task_path':        obj.getPath(),
+            'task_title':       obj.attr('outlookid'),
+            'exec_onchange':    True,
+            'exec_zms2sql':     False,
+            'exec_opensearch':  False,
+            'added_dt':         datetime.now(),
+            'processed_dt':     None,
+        }
+        return cls.model_validate(mapping)
