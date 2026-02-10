65b3716b6679ea0d04885377279df995519bb84c
Christian Meier <christian.meier@unibe.ch>
2026-02-10 12:29:23 +0100

    refact(sqlmodel): Renamed to ZMSSurveyJSSubmissions (#30)
    Clarified naming for data storage of submissions to use ZMSSurveyJS for the zms2sql model later on.
    
    See also https://github.com/idasm-unibe-ch/unibe-cms/pull/1149/commits/cc3cb050e6ab98288d6858f46c531599a729b447

--- src/zms/unibe/formulator/sqlmodels/ZMSSurveyJSSubmissions.py
+++ src/zms/unibe/formulator/sqlmodels/ZMSSurveyJSSubmissions.py
@@ -5,7 +5,7 @@ from sqlmodel import SQLModel, Field, Column, DateTime
 from zms.unibe.utils.helpers import parse_uuid, local_timezone
 
 
-class ZMSSurveyJS(SQLModel, table=True):
+class ZMSSurveyJSSubmissions(SQLModel, table=True):
     __table_args__ = {'extend_existing': True}
     id: int | None = Field(default=None, primary_key=True)
     survey_uuid: UUID
