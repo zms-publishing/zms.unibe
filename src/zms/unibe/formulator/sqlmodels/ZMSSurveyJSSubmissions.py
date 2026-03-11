2ffd71e72b925688256ba2a69e685d868b63bcee
Christian Meier <christian.meier@unibe.ch>
2026-03-11 19:50:54 +0100

    refact(survey): Added JSON sanitization to from_submit

--- src/zms/unibe/formulator/sqlmodels/ZMSSurveyJSSubmissions.py
+++ src/zms/unibe/formulator/sqlmodels/ZMSSurveyJSSubmissions.py
@@ -1,3 +1,5 @@
+import json
+
 from datetime import datetime
 from uuid import UUID
 from sqlmodel import SQLModel, Field, Column, DateTime
@@ -15,11 +17,17 @@ class ZMSSurveyJSSubmissions(SQLModel, table=True):
 
     @classmethod
     def from_submit(cls, survey):
+        try:
+            # sanitize submitted data
+            data_json = json.loads(survey.REQUEST.form.get('data'))
+        except Exception as e:
+            raise ValueError(f"Invalid JSON data in survey submission: {e}")
+
         mapping = {
             'id': None,
             'survey_uuid': parse_uuid(survey.get_uid()),
             'survey_path': survey.getPath(),
-            'survey_data_json': survey.REQUEST.form.get('data'),
+            'survey_data_json': json.dumps(data_json, indent=4, sort_keys=True, default=str),
             'survey_submitted_at': local_timezone(datetime.now()),
         }
-        return cls.model_validate(mapping)
+        return cls.model_validate(mapping), data_json
