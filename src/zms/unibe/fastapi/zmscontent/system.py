2ab691e987fc4e63a93a97e95973cc5484202185
Christian Meier <christian.meier@unibe.ch>
2026-05-07 20:02:54 +0200

    feat(fastapi): Added endpoints /v1/zms/system/packages + /metaobjs

---
+++ src/zms/unibe/fastapi/zmscontent/system.py
@@ -0,0 +1,60 @@
+import os
+from datetime import datetime
+from uuid import UUID
+
+from fastapi import APIRouter, Query, HTTPException
+from sqlmodel import Session, select, inspect
+
+from zms.unibe.utils.zope.context import create_zope_app_context, get_zmsindex
+from zms.unibe.utils.enums import ContentModel
+from zms.unibe.fastapi.meta import Tags
+
+router = APIRouter(prefix="/zms", tags=[Tags.system])
+
+
+@router.get(
+    path="/system/packages",
+    summary="Get installed Python packages",
+)
+def get_installed_python_packages(
+        portal_master: str | None = Query(os.getenv('PORTAL_MASTER', '/myzmsx/content'),
+                                          description="Portal master with ZMSIndex"),
+):
+    context = create_zope_app_context()
+    zmsindex = get_zmsindex(portal_master, context)
+    content = zmsindex.content
+    
+    packages_html = content.manage_customizeInstalledProducts()
+    packages = packages_html.splitlines()
+    packages = [content.zms_version(custom=True)] + packages
+    # TODO: filter out HTML tags and other unwanted stuff
+    
+    return packages
+
+
+@router.get(
+    path="/system/metaobjs",
+    summary="Get installed Meta objects",
+)
+def get_installed_meta_objects(
+        content_model: ContentModel | None = None,
+        portal_master: str | None = Query(os.getenv('PORTAL_MASTER', '/myzmsx/content'),
+                                          description="Portal master with ZMSIndex"),
+):
+    context = create_zope_app_context()
+    zmsindex = get_zmsindex(portal_master, context)
+    content = zmsindex.content
+ 
+    metaobjs = {}
+    for id in content.getMetaobjIds():
+        data = {}
+        data['revision'] = content.getMetaobjRevision(id)
+        data['attributes'] = content.getMetaobjAttrs(id)
+        metaobjs[id] = data
+
+    if content_model in metaobjs:
+        return {content_model: metaobjs[content_model]}
+    elif content_model:
+        raise HTTPException(status_code=404,
+                            detail=f"Content model '{content_model.name}' not found.")
+    return metaobjs
