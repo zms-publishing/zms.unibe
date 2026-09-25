6ba6485313c98a321ac26f06be4c0d22b3d0757d
Christian Meier <christian.meier@unibe.ch>
2026-09-25 12:36:01 +0200

    refact(fastapi): Combined VHOST /v1/zms/domain/mappings

--- src/zms/unibe/fastapi/zmscontent/system.py
+++ src/zms/unibe/fastapi/zmscontent/system.py
@@ -2,14 +2,16 @@ from fastapi import APIRouter, HTTPException
 
 from zms.unibe.utils.zope.context import create_zope_app_context
 from zms.unibe.fastapi.meta import Tags
+from zms.unibe.utils.enums import VirtualHosting
 
 from Products.zms.standard import get_installed_packages
+from Products.ZODBMountPoint.MountedObject import manage_getMountStatus
 
-router = APIRouter(prefix="/zms/system", tags=[Tags.system])
+router = APIRouter(prefix="/zms", tags=[Tags.system])
 
 
 @router.get(
-    path="/packages",
+    path="/python/packages",
     summary="Get installed Python packages",
 )
 def get_installed_python_packages(
@@ -54,15 +56,40 @@ def get_zope_object_databases():
     summary="Get virtual host Domain/Path mappings",
 )
 def get_virtual_hosting_mappings(
-
+    filter_by: VirtualHosting | None = None,
 ):
     context = create_zope_app_context()
-    return context.virtual_hosting.lines
+    flipped = False
+    
+    match filter_by:
+        case 'subdomains':
+            items = context.virtual_hosting.fixed_map.items()
+        case 'wildcards':
+            items = context.virtual_hosting.sub_map.items()
+        case 'paths':
+            items = {**context.virtual_hosting.fixed_map,
+                     **context.virtual_hosting.sub_map}.items()
+            flipped = True
+        case _:
+            return context.virtual_hosting.lines
 
+    mappings = {
+        ('*.'+host if host in context.virtual_hosting.sub_map.keys() else host):
+            '/' + ('/'.join(reversed(mapping.get(None, [])))).replace('//', '')
+        for host, mapping in items
+    }
+
+    if flipped:
+        tmp = {}
+        for host, path in mappings.items():
+            tmp.setdefault(path, []).append(host)
+        mappings = tmp
+    
+    return mappings
 
 @router.post(
-    path="/mappings",
-    summary="Set virtual hosting mappings",
+    path="/domain/mappings",
+    summary="Set virtual host Domain/Path mappings",
 )
 def set_virtual_hosting_mappings(
         mapping: str
@@ -85,25 +112,3 @@ def set_virtual_hosting_mappings(
             context.virtual_hosting.set_map(mappings)
 
     return context.virtual_hosting.lines
-
-  
-@router.get(
-    path="/subdomains",
-    summary="Get virtual hosting subdomains",
-)
-def get_virtual_hosting_subdomains(
-
-):
-    context = create_zope_app_context()
-    return context.virtual_hosting.fixed_map
-
-
-@router.get(
-    path="/wildcards",
-    summary="Get virtual hosting wildcards",
-)
-def get_virtual_hosting_wildcards(
-
-):
-    context = create_zope_app_context()
-    return context.virtual_hosting.sub_map
